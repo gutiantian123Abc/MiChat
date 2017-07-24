f34a4e325ab80464ca8204391bf7ab339642fdae
gutiantian123Abc <william.guxiangtian@gmail.com>
2017-07-24 15:15:59 -0400

    first commit

---
+++ app/src/main/java/mimi/michat/api/BaseApp.java
@@ -0,0 +1,97 @@
+package mimi.michat.api;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.multidex.MultiDexApplication;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by xiangtiangu on 7/24/17.
+ *   @Application Base Class
+ */
+
+public class BaseApp extends MultiDexApplication{
+    public static List<Activity> activities = new LinkedList<>();
+
+    //The followings are global variables for the whole application, for better use of resources, avoid waste
+    private static Context mContext;//上下文
+    private static Thread mMainThread;//主线程
+    private static long mMainThreadId;//主线程id
+    private static Looper mMainLooper;//循环队列
+    private static Handler mHandler;//主线程Handler
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = getApplicationContext();
+        mMainThread = Thread.currentThread();
+        mMainThreadId = android.os.Process.myTid();
+        mHandler = new Handler();
+    }
+
+
+    /**
+     * Fully exit the program
+     */
+
+    public static void exit() {
+        for(Activity activity : activities) {
+            activity.finish();
+        }
+    }
+
+    /**
+     * Restart
+     */
+    public static void restart() {
+        Intent intent = mContext.getPackageManager().getLaunchIntentForPackage(mContext.getPackageName());
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        mContext.startActivity(intent);
+    }
+
+
+    public static Context getmContext() {
+        return mContext;
+    }
+
+    public static void setmContext(Context mContext) {
+        BaseApp.mContext = mContext;
+    }
+
+    public static Thread getmMainThread() {
+        return mMainThread;
+    }
+
+    public static void setmMainThread(Thread mMainThread) {
+        BaseApp.mMainThread = mMainThread;
+    }
+
+    public static long getmMainThreadId() {
+        return mMainThreadId;
+    }
+
+    public static void setmMainThreadId(long mMainThreadId) {
+        BaseApp.mMainThreadId = mMainThreadId;
+    }
+
+    public static Looper getmMainLooper() {
+        return mMainLooper;
+    }
+
+    public static void setmMainLooper(Looper mMainLooper) {
+        BaseApp.mMainLooper = mMainLooper;
+    }
+
+    public static Handler getmHandler() {
+        return mHandler;
+    }
+
+    public static void setmHandler(Handler mHandler) {
+        BaseApp.mHandler = mHandler;
+    }
+}
